9d2a472b7fe62668aee7349e8da83ab85a39a535
Diana Mauricio <dwmauricio@g.ucla.edu>
2021-07-14 20:37:12 -0700

    Added bootstrap + Jquery

--- thirdpartyapis/dayplanner.js
+++ thirdpartyapis/dayplanner.js
@@ -1,65 +1,64 @@
-$(document).ready(function () {
-
-    var x = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
-
-
-
-    //moment.js to put current date on jumbotron
-    var currentDate = moment().format("dddd, MMMM Do YYYY");
-    $("#currentDay").text(currentDate);
-    var currentTime = moment().format("H");
-
-    //forEach loops through x[], pushes "time" elements to new startTime[] in the "00 AM/PM" format
-    x.forEach(function (time, index) {
-        var startTime = [];
-        startTime.push(moment().hour(time).format("h A"));
-        // console.log(parseInt(startTime));
-        // console.log(moment().hour());
-
-        //Declare new var(s) and initialize to new el's
-        var newDiv = $("<div>");
-        var newSpan = $("<span>");
-        var newTextInput = $("<input>");
-        var newBtn = $("<button>");
-
-
-        //appends new el's to container, parent or sibling respectively. Adds B/S classes to new el's 
-        $(".container").append(newDiv);
-        $(".container").addClass("mb-5");
-
-        newDiv.addClass("time-block input-group input-group-prepend");
-        newDiv.attr("data-val", index);
-        newDiv.append(newSpan);
-        newSpan.addClass("input-group-text");
-        newSpan.addClass("start-time")
-        newSpan.text(startTime);
-        newDiv.append(newTextInput);
-        newTextInput.attr("type", "text");
-        newTextInput.attr("data", "data-input" + index);
-        newTextInput.addClass("form-control")
-        newTextInput.attr("data", "data-text" + index);
-        newDiv.append(newBtn);
-        newBtn.addClass("btn btn-outline-secondary");
-
-        /* END OF FOREACH()*/
+// Wait until Jquery is ready
+$( document ).ready(function() {
+    //Display current date
+    //Variable to append cols
+  var a= moment().format('dddd MMMM Do YYYY, h:mm');
+    $("#display-date").text(a)
+    var row = ""
+    //loop to dispaly 9am-18pm
+      for (var i= 9 ; i<=18; i++){
+        // Creation of the row elements
+        row = $(`<div class="row">`)
+        col1 = $(`<div class ="col-lg-2 hour">${displayAmorPm(i)}</div>`)
+        col2 = $(`<div class ="col-lg-8 inputcontent"><input data-input="${i}" id="inputText${i}" class="form-control inputText" type="text" name="userInput"></div>`)
+        col3 = $(`<div class ="col-lg-2"><button data-id="${i}" id="savePlanner" class="btn btn-success btn-block"><i class="fas fa-save"></i> Save</button></div>`)
+        row.append(col1)
+        row.append(col2)
+        row.append(col3)
+        $("#display-planner").append(row)
+        getlocalStorage(i)
+      }
+     $("button.btn.btn-success").click(function(e){
+     var id = $(this).data("id")
+     var inputText = $(this).parent().siblings().find("input").val()
+     localStorage.setItem(id,inputText)
+     })
+    //  Convert Am to Pm
+     function displayAmorPm(hour){
+       var b=""
+       if(hour<=12){
+         b= "AM"
+       }else{
+         b="PM"
+       }
+       hour = hour % 12
+       hour = hour ? hour : 12
+       return hour + " " + b
+     }
     });
-
-    //click event 
-    $("button").on("click", function (event) {
-        event.preventDefault();
-
-        var textInput = $(this).siblings("input").attr("data");
-        var textVal = $(this).siblings("input").val();
-        localStorage.setItem(textInput, textVal);
-        var lsVal = localStorage.getItem(textInput);
-        $(this).siblings("input").text(lsVal);
-    });
-
-    //updated width of time block span and changed btn text to "SAVE"
-    // Consider using fontawesome icon for save feature
-    $(".input-group-text").css("width", "100px");
-    $("button").text("SAVE"); // update with icon from font-awesome
-
-
-})
+  
+     function getlocalStorage(hour){
+       let inputval = localStorage.getItem(hour)
+       if(true){
+        //  $("input").data(`input${hour}`)
+        var text= $(`input#inputText${hour}`).val(inputval)
+        console.log(text)
+       }
+     }
+     //Apdate color
+     function updateColor(){
+       var hour = new Date().getHours();
+       for (var i= 9 ; i<=18; i++){
+         console.log(hour,i)
+         if(hour==i ) {
+          $(`#inputText${i}`).css("background","red")
+         }else  if(hour<i ){
+          
+           $(`#inputText${i}`).css("background","lightblue")
+  
+         }
+       }
+     }
+     setInterval(function(){
+       updateColor()
+     },1000)
